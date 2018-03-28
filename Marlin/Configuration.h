84c379154aaeadd807941631978c526daca07c5d
djfye <djfye2007@gmail.com>
2018-03-28 00:24:07 -0500

    Machine updates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -819,10 +819,10 @@
 #define XY_PROBE_SPEED 8000
 
 // Speed for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
+#define Z_PROBE_SPEED_FAST 5                 // HOMING_FEEDRATE_Z
 
 // Speed for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
+#define Z_PROBE_SPEED_SLOW 3                // (Z_PROBE_SPEED_FAST / 2)
 
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
